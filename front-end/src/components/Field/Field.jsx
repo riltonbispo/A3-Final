1d867f89015751589832ebbcb54cc01f153757e7
rilton bispo <riltondev@gmail.com>
2023-11-26 12:10:42 -0300

    :recycle: Campos controlados

--- front-end/src/components/Field/Field.jsx
+++ front-end/src/components/Field/Field.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import style from './Field.module.css'
 
-const Field = ({label, type, placeholder}) => {
+const Field = ({ label, type, placeholder }) => {
+
+  const handleChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
   return (
     <div className={style.field}>
       <label>{label}</label>
-      <input type={type} placeholder={placeholder} />
+      <input type={type} placeholder={placeholder} onChange={handleChange} />
     </div>
   )
 }
--- front-end/src/routes/SignUp/SignUp.jsx
+++ front-end/src/routes/SignUp/SignUp.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react';
 import Footer from "../../partials/Footer";
 import Header from "../../partials/Header";
 import style from './SignUp.module.css'
@@ -7,6 +7,22 @@ import GameImage from '../../assets/img/image-login.png'
 import Field from '../../components/Field';
 
 const SignUp = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  const handleEmailChange = (value) => {
+    setEmail(value);
+  };
+
+  const handlePasswordChange = (value) => {
+    setPassword(value);
+  };
+
+  const handleConfirmPasswordChange = (value) => {
+    setConfirmPassword(value);
+  };
+
   return (
     <div className={style.login}>
       <Header />
@@ -18,9 +34,9 @@ const SignUp = () => {
           <form className={style.form}>
             <span className={style.title}>Criar Conta</span>
             <p className={style.description}>Crie sua conta e acesse todo o seu catálogo de jogos</p>
-            <Field  label='Email' type="email" placeholder="exemple@email.com"/>
-            <Field  label='Senha' type="password" placeholder="******"/>
-            <Field  label='Confirmar Senha' type="password" placeholder="******"/>
+            <Field  label='Email' type="email" placeholder="exemple@email.com" onChange={handleEmailChange}/>
+            <Field  label='Senha' type="password" placeholder="******" onChange={handlePasswordChange}/>
+            <Field  label='Confirmar Senha' type="password" placeholder="******" onChange={handleConfirmPasswordChange}/>
           </form>
           <div className={style.buttons}>
             <Button link="index.html" label="Já Possuo Conta" />
