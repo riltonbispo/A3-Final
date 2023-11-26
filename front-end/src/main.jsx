4e23c17738408ed3c0931c6e69f5ae5628db9daa
rilton bispo <riltondev@gmail.com>
2023-11-26 08:45:47 -0300

    :heavy_plus_sign: React router dom

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -1,11 +1,31 @@
-import React from "react";
+import React from 'react';
 import ReactDOM from "react-dom/client";
 import "./styles/variables.css";
 import "./styles/global-styles.css";
 import App from "./App.jsx";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import SignUp from './routes/SignUp';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>root</div>
+  },
+  {
+    path: "signup",
+    element: <SignUp/>
+  },
+  {
+    path: "/home",
+    element: <App />,
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
