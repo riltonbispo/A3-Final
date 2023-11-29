26bfdd69b8de0e74eabd68540604544b83803412
riltonbispo <riltondev@gmail.com>
2023-11-29 09:25:38 -0300

    feat: platform modal

--- front-end/src/partials/HeaderHome/HeaderHome.jsx
+++ front-end/src/partials/HeaderHome/HeaderHome.jsx
@@ -5,28 +5,31 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Logo from '../../assets/img/ECHO-black.svg'
 import GameModal from '../../components/ModalGame/GameModal';
+import PlatformModal from '../../components/PlatformModal/PlatformModal';
 
 
 const HeaderHome = () => {
-  const [modalShow, setModalShow] = useState(false);
+  const [showModalGame, setShowModalGame] = useState(false);
+  const [showModalPlatform, setShowModalPlatform] = useState(false);
 
   return (
     <>
-      <Navbar expand="lg" className="bg-body-tertiary">
+      <Navbar expand="lg" className="bg-body-tertiary border-bottom">
         <Container>
           <Navbar.Brand>
             <img src={Logo} alt="" />
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Button label='Adicionar Jogo' onClick={() => { setModalShow(true); }} />
-              <Button label='Gerenciar Plataformas' secondary />
+          <Navbar.Collapse id="basic-navbar-nav" className='justify-content-end'>
+            <Nav className="me-auto" style={{gap: '0.5rem'}}>
+              <Button label='Adicionar Jogo' onClick={() => { setShowModalGame(true); }} />
+              <Button label='Gerenciar Plataformas' secondary  onClick={() => { setShowModalPlatform(true); }}/>
             </Nav>
           </Navbar.Collapse>
         </Container>
       </Navbar>
-      <GameModal show={modalShow} onHide={() => setModalShow(false)} />
+      <GameModal show={showModalGame} onHide={() => setShowModalGame(false)} />
+      <PlatformModal show={showModalPlatform} onHide={() => setShowModalPlatform(false)} />
     </>
   )
 }
